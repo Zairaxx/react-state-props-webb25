8fb1d39d4592221b3690ca7f15b2ff7e78b73b86
Zairaxx <brandontsegai@gmail.com>
2025-11-04 13:42:53 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react'
+import './App.css'
+import Profile from './components/Profile'
+
+function App() {
+
+
+  const [personer, setPersoner] = useState([
+    {
+      fornamn: "Anna",
+      efternamn: "Svensson",
+      alder: 25,
+      hobby: "Fotboll"
+    },
+    {
+      fornamn: "Erik",
+      efternamn: "Johansson",
+      alder: 30,
+      hobby: "Löpning"
+    },
+    {
+      fornamn: "Maria",
+      efternamn: "Karlsson",
+      alder: 28,
+      hobby: "Måla"
+    },
+    {
+      fornamn: "Lars",
+      efternamn: "Nilsson",
+      alder: 35,
+      hobby: "Cykling"
+    },
+    {
+      fornamn: "Emma",
+      efternamn: "Andersson",
+      alder: 22,
+      hobby: "Läsa"
+    },
+  {
+      fornamn: "Hanna",
+      efternamn: "Andersson",
+      alder: 22,
+      hobby: "Spela TV-spel"
+  },
+{
+      fornamn: "Hanna",
+      efternamn: "Andersson",
+      alder: 22,
+      hobby: "Spela TV-spel"
+  }])
+
+
+  return (
+    <>
+    <h1>React state & Props - Webb25 - Antal personer: {personer.length}</h1>
+    {/* <button onClick={() => {
+      setPersoner([...personer, {
+      fornamn: "Hanna",
+      efternamn: "Andersson",
+      alder: 22,
+      hobby: "Spela TV-spel"
+  }])
+    }}>Lägg till person</button> */}
+    <ul>
+      {personer.map(person => <Profile data={person}/>)}
+    </ul>
+    </>
+  )
+}
+
+export default App
